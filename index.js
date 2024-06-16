73372ff087177effee81ceb8fa638e3deda1915e
Aakash Saini <aakashsaini31012002@gmail.com>
2024-06-16 07:43:24 +0530

    c

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const PORT = 8000;
 dotenv.config();
 
 const app = express();
-app.use(cors());
+const corsConfig = {
+    origin : "*",
+    credential: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsConfig));
 app.use(bodyParser.json({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
 
